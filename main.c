7c73d37b8d6845530fbe571833ae8ba2ba35b626
kaique <kaiquefarias2009@gmail.com>
2020-02-21 17:05:29 -0300

    Calculadora pronta para entrega

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    char op;
+    int operando1, operando2;
+
+    printf("Qual operacao realizar ?\n");
+    scanf("%c", &op);
+    printf("Qual o primeiro operando ?\n");
+    scanf("%d", &operando1);
+    printf("Qual o primeiro operando ?\n");
+    scanf("%d", &operando2);
+    switch (op){
+        case '+':
+            printf("%d + %d = %d", operando1, operando2, operando1 + operando2);
+            break;
+        case '-':
+            printf("%d - %d = %d", operando1, operando2, operando1 - operando2);
+            break;
+        case '*':
+            printf("%d * %d = %d", operando1, operando2, operando1 * operando2);
+            break;
+        case '/':
+            printf("%d / %d = %d", operando1, operando2, operando1 / operando2);
+            break;
+    
+    
+    
+    
+    }
+
+    return 0;
+}
